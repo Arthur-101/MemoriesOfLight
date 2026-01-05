fb62f3c836ef26bd4b974d5f08605b3f304396e3
Saurav Kumar <arthur14202000@gmail.com>
2026-01-05 08:59:23 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCfj-_tfUxmEk6COpVejaPmULkI8PhuOqU",
+    authDomain: "memoriesoflight-8cfa5.firebaseapp.com",
+    projectId: "memoriesoflight-8cfa5",
+    storageBucket: "memoriesoflight-8cfa5.firebasestorage.app",
+    messagingSenderId: "475651062020",
+    appId: "1:475651062020:web:0a53aab37d691e069bf851",
+};
